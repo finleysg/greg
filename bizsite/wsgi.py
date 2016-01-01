b6404f29d0bfbf8740a96396df59363b19c24073
stuart <finleysg@gmail.com>
2016-01-01 16:15:51 -0600

    change to local_settings

--- bizsite/wsgi.py
+++ bizsite/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bizsite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bizsite.local_settings")
 
 application = get_wsgi_application()
