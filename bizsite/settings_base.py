c37683b1b24611b0d038531fe0a6d34dcf06fd8a
stuart <finleysg@gmail.com>
2016-02-12 08:57:45 -0600

    roll back s3 changes

--- bizsite/settings_base.py
+++ bizsite/settings_base.py
@@ -104,3 +104,16 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'var/www/static')
+
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'var/www/media')
+
+MEDIA_URL = '/media/'
